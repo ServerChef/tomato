5b982bac84b86202944337339eabc698f67d7ba4
Damodar Dahal <underscoredam@gmail.com>
2017-01-09 15:54:18 -0600

    Added default models

--- betterboy/models.py
+++ betterboy/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+
+class Site(models.Model):
+    site_title = models.CharField(max_length=200)
+    create_time = models.DateTimeField("Create Time")
+
+
+class Plugin(models.Model):
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    software_name = models.CharField(max_length=200)
+    create_time = models.DateTimeField("Create Time")
