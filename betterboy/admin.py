0a44b41bc824a19e2acc6e5c9cf8cbb1b4de83ef
Damodar Dahal <underscoredam@gmail.com>
2017-01-09 16:13:42 -0600

    Show models on admin

--- betterboy/admin.py
+++ betterboy/admin.py
@@ -1,2 +1,6 @@
 from django.contrib import admin
 
+from .models import Site, Plugin
+
+admin.site.register(Site)
+admin.site.register(Plugin)
--- betterboy/models.py
+++ betterboy/models.py
@@ -5,8 +5,14 @@ class Site(models.Model):
     site_title = models.CharField(max_length=200)
     create_time = models.DateTimeField("Create Time")
 
+    def __str__(self):
+        return self.site_title
+
 
 class Plugin(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     software_name = models.CharField(max_length=200)
     create_time = models.DateTimeField("Create Time")
+
+    def __str__(self):
+        return self.software_name
