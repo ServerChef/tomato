dfb1c031a76c5e8111ff7b5b6f0dc8830009abe4
Damodar Dahal <underscoredam@gmail.com>
2017-01-09 18:40:00 -0600

    Use default value for create_time

--- betterboy/models.py
+++ betterboy/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 
 class WebSite(models.Model):
     site_title = models.CharField(max_length=200)
-    create_time = models.DateTimeField("Create Time")
+    create_time = models.DateTimeField("Create Time", default=timezone.now)
 
     def to_dict(self):
         return {
@@ -18,7 +19,7 @@ class WebSite(models.Model):
 class Plugin(models.Model):
     site = models.ForeignKey(WebSite, on_delete=models.CASCADE)
     software_name = models.CharField(max_length=200)
-    create_time = models.DateTimeField("Create Time")
+    create_time = models.DateTimeField("Create Time", default=timezone.now)
 
     def __str__(self):
         return self.software_name
